2ced009a782b5aeaece5caf13898552f3aed6682
Roberto Focosi <rfocosi@gmail.com>
2019-09-28 00:49:03 -0300

    Fix seconds in month

--- workspace/dos-utils/header/version.h
+++ workspace/dos-utils/header/version.h
@@ -1,1 +1,1 @@
-static const unsigned char TOOLS_VERSION[]="v1.0";
+static const unsigned char TOOLS_VERSION[]="v1.1";
--- workspace/dos-utils/sleep.c
+++ workspace/dos-utils/sleep.c
@@ -23,8 +23,8 @@
 static int show_help;
 static int verbose_mode;
 
-static unsigned long current_seconds = 0l;
-static unsigned long scheduled_seconds = 0l;
+static unsigned long long current_seconds = 0l;
+static unsigned long long scheduled_seconds = 0l;
 
 byte* pbuffer = BUFFER;
 
@@ -57,13 +57,29 @@ static void usage () {
   exit();
 }
 
-static unsigned long to_seconds(int year, int month, int day, int hour, int minute, int second) {
+static unsigned long seconds_in_month(int month, int is_leap_year) {
+  switch(month) {
+    case 2:
+      return (is_leap_year ? SECS_IN_MONTH_29 : SECS_IN_MONTH_28);
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return SECS_IN_MONTH_31;
+  }
+  return SECS_IN_MONTH_30;
+}
+
+static unsigned long long to_seconds(int year, int month, int day, int hour, int minute, int second) {
   int is_leap_year = ((year & 3) == 0);
-  unsigned long SecondsInYears = (year - EPOCH_YEAR) * (is_leap_year ? SECS_IN_LYEAR : SECS_IN_YEAR);
-  unsigned long SecondsInMonths = month * SECS_IN_MONTH_28;
-  unsigned long SecondsInDays = day * SECS_IN_DAY;
-  unsigned long SecondsInHours = hour * SECS_IN_HOUR;
-  unsigned long SecondsInMinutes = minute * SECS_IN_MINUTE;
+  unsigned long long SecondsInYears = (year - EPOCH_YEAR) * (is_leap_year ? SECS_IN_LYEAR : SECS_IN_YEAR);
+  unsigned long long SecondsInMonths = month * seconds_in_month(month, is_leap_year);
+  unsigned long long SecondsInDays = day * SECS_IN_DAY;
+  unsigned long long SecondsInHours = hour * SECS_IN_HOUR;
+  unsigned long long SecondsInMinutes = minute * SECS_IN_MINUTE;
 
   return
     SecondsInYears +
